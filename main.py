86e83703db744f771c1b790a23a0ee373935eab7
valyamih1 <valyamih1@gmail.com>
2022-12-02 19:28:07 +0300

    working LSTM with google data

--- main.py
+++ main.py
@@ -2,35 +2,40 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-payment_data = pd.read_csv('data/dataset.csv')
-print(payment_data.head())
+google_stock_data = pd.read_csv('data/GOOG.csv')
+print(google_stock_data.head())
 
-payment_data.info()
+google_stock_data.info()
 
-payment_data = payment_data[['PAY_DATE', 'PAY']] # Extracting required columns
-payment_data['PAY_DATE'] = pd.to_datetime(payment_data['PAY_DATE'].apply(lambda x: x.split()[0])) # Selecting only date
-payment_data.set_index('PAY_DATE', drop=True, inplace=True) # Setting date column as index
-print(payment_data.head())
+google_stock_data = google_stock_data[['date','open','close']] # Extracting required columns
+google_stock_data['date'] = pd.to_datetime(google_stock_data['date'].apply(lambda x: x.split()[0])) # Selecting only date
+google_stock_data.set_index('date',drop=True,inplace=True) # Setting date column as index
+print(google_stock_data.head())
 
 fg, ax =plt.subplots(1,2,figsize=(20,7))
-ax[0].plot(payment_data['PAY'], label='PAY', color='green')
-ax[0].set_xlabel('PAY_DATE',size=15)
-ax[0].set_ylabel('PAY',size=15)
+ax[0].plot(google_stock_data['open'],label='Open',color='green')
+ax[0].set_xlabel('Date',size=15)
+ax[0].set_ylabel('Price',size=15)
 ax[0].legend()
+
+ax[1].plot(google_stock_data['close'],label='Close',color='red')
+ax[1].set_xlabel('Date',size=15)
+ax[1].set_ylabel('Price',size=15)
+ax[1].legend()
 
 fg.show()
 
 from sklearn.preprocessing import MinMaxScaler
 MMS = MinMaxScaler()
-payment_data[payment_data.columns] = MMS.fit_transform(payment_data)
+google_stock_data[google_stock_data.columns] = MMS.fit_transform(google_stock_data)
 
-print(payment_data.shape)
+print(google_stock_data.shape)
 
-training_size = round(len(payment_data) * 0.80) # Selecting 80 % for training and 20 % for testing
+training_size = round(len(google_stock_data) * 0.80) # Selecting 80 % for training and 20 % for testing
 print(training_size)
 
-train_data = payment_data[:training_size]
-test_data  = payment_data[training_size:]
+train_data = google_stock_data[:training_size]
+test_data  = google_stock_data[training_size:]
 
 print(train_data.shape, test_data.shape)
 
@@ -74,23 +79,30 @@ print(test_predicted[:5])
 test_inverse_predicted = MMS.inverse_transform(test_predicted) # Inversing scaling on predicted data
 print(test_inverse_predicted[:5])
 
-gs_slic_data = pd.concat([payment_data.iloc[-202:].copy(), pd.DataFrame(test_inverse_predicted, columns=['open_predicted', 'close_predicted'], index=payment_data.iloc[-202:].index)], axis=1)
+gs_slic_data = pd.concat([google_stock_data.iloc[-202:].copy(),pd.DataFrame(test_inverse_predicted,columns=['open_predicted','close_predicted'],index=google_stock_data.iloc[-202:].index)], axis=1)
 
-gs_slic_data[['PAY']] = MMS.inverse_transform(gs_slic_data[['PAY']])
+gs_slic_data[['open','close']] = MMS.inverse_transform(gs_slic_data[['open','close']])
 
 print(gs_slic_data.head())
 
-gs_slic_data[['PAY','PAY_predicted']].plot(figsize=(10,6))
+gs_slic_data[['open','open_predicted']].plot(figsize=(10,6))
 plt.xticks(rotation=45)
 plt.xlabel('Date',size=15)
 plt.ylabel('Stock Price',size=15)
 plt.title('Actual vs Predicted for open price',size=15)
 plt.show()
 
+gs_slic_data[['close','close_predicted']].plot(figsize=(10,6))
+plt.xticks(rotation=45)
+plt.xlabel('Date',size=15)
+plt.ylabel('Stock Price',size=15)
+plt.title('Actual vs Predicted for close price',size=15)
+plt.show()
+
 gs_slic_data = gs_slic_data.append(pd.DataFrame(columns=gs_slic_data.columns,index=pd.date_range(start=gs_slic_data.index[-1], periods=11, freq='D', closed='right')))
 print(gs_slic_data['2021-06-09	':'2021-06-16'])
 
-upcoming_prediction = pd.DataFrame(columns=['PAY'],index=gs_slic_data.index)
+upcoming_prediction = pd.DataFrame(columns=['open','close'],index=gs_slic_data.index)
 upcoming_prediction.index=pd.to_datetime(upcoming_prediction.index)
 
 curr_seq = test_seq[-1:]
@@ -101,15 +113,25 @@ for i in range(-10,0):
   curr_seq = np.append(curr_seq[0][1:],up_pred,axis=0)
   curr_seq = curr_seq.reshape(test_seq[-1:].shape)
 
-upcoming_prediction[['PAY']] = MMS.inverse_transform(upcoming_prediction[['PAY']])
+upcoming_prediction[['open','close']] = MMS.inverse_transform(upcoming_prediction[['open','close']])
 
 fg,ax=plt.subplots(figsize=(10,5))
-ax.plot(gs_slic_data.loc['2021-04-01':,'PAY'],label='Current Open Price')
-ax.plot(upcoming_prediction.loc['2021-04-01':,'PAY'],label='Upcoming Open Price')
+ax.plot(gs_slic_data.loc['2021-04-01':,'open'],label='Current Open Price')
+ax.plot(upcoming_prediction.loc['2021-04-01':,'open'],label='Upcoming Open Price')
 plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
 ax.set_xlabel('Date',size=15)
-ax.set_ylabel('Price',size=15)
-ax.set_title('Upcoming PAYMENT prediction',size=15)
+ax.set_ylabel('Stock Price',size=15)
+ax.set_title('Upcoming Open price prediction',size=15)
+ax.legend()
+fg.show()
+
+fg,ax=plt.subplots(figsize=(10,5))
+ax.plot(gs_slic_data.loc['2021-04-01':,'close'],label='Current close Price')
+ax.plot(upcoming_prediction.loc['2021-04-01':,'close'],label='Upcoming close Price')
+plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
+ax.set_xlabel('Date',size=15)
+ax.set_ylabel('Stock Price',size=15)
+ax.set_title('Upcoming close price prediction',size=15)
 ax.legend()
 fg.show()
 
